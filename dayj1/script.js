32e05850f038d1302ddc401e79a0fec837e62949
koushikyadav998 <koushikyadav998@gmail.com>
2026-04-13 09:17:15 +0530

    added file

---
+++ dayj1/script.js
@@ -0,0 +1,48 @@
+var num1 = 10;
+var c1=0;
+//console.log(num1 + num2);
+//console.log(typeof num1);
+//if(num1 === num2) console.log(-1);
+ //console.log(2 ** 3);
+ 
+//  for( var i = 2;i<=num1;i++)
+//  {   var c = 0;
+//     for(var j = 2;j <= i;j++)
+//     {
+//         if(i % j == 0)
+//         {
+//             c++;
+//         }
+//     }
+//     if(c == 1)
+//     {
+
+//         console.log(i);
+//     }
+//  }
+// const obj={
+//     id:1,
+//     name:"koushik",
+//     mydata:[30,"hello","bye bye"]
+// }
+// console.log(obj.l);
+// obj.l="Hyd"
+// console.log(obj);
+// console.log(obj.a);
+// obj.a=[20,30,40]
+// console.log(obj);
+// console.log(obj.mydata);
+const a = [
+{
+    id : 1,
+    name:"koushik",
+    salary:56.0
+},
+{
+    id:2,
+    name:"kanna",
+    salary:30.0
+}  
+]
+console.log(a[1].name);
+
